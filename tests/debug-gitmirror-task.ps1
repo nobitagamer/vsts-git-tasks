321ef60ffb1864b0e8bb8682f2d925799bb402c2
Nguyen Khac Trieu <trieunk@yahoo.com>
2017-04-14 11:29:30 +0700

    chore: Do not tag on develop branch

--- tests/debug-gitmirror-task.ps1
+++ tests/debug-gitmirror-task.ps1
@@ -1,9 +1,9 @@
-﻿Save-Module -Name VstsTaskSdk -Path .\ps_modules\
+﻿# Save-Module -Name VstsTaskSdk -Path .\ps_modules\
 Save-Module -Name posh-git -Path .\ps_modules\
 
 Install-Module posh-git -Verbose -Scope CurrentUser
 
-Import-Module .\ps_modules\VstsTaskSdk
+# Import-Module .\ps_modules\VstsTaskSdk
 Import-Module .\ps_modules\posh-git
 
 Invoke-VstsTaskScript -ScriptBlock ([scriptblock]::Create('. .\gitmirror-task\task.ps1')) -Verbose
